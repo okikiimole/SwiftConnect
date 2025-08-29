f33778a938b4c168ce1bf6d4a244d487ff1731fe
okikiimole <okikiimoleglobalenterprise@gmail.com>
2025-08-29 10:56:07 +0100

    Fixed the Deployment issue

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import TestimonialsCarousel from "../components/TestimonialsCarousel";
